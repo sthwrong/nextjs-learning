bf089a16a22309435c3a71c0d7db6de37b983cc0
hiepnv <hiepnv@hblab.vn>
2021-02-26 16:12:35 +0700

    Test config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
-const ENV = process.env.NEXT_ENV ? process.env.NEXT_ENV : "development";
-
 require("dotenv").config({
   path: "./env/.env.development",
 });
 
 module.exports = {
   env: {
-    NEXT_ENV: ENV,
-    HIEP_TEST_ENV: process.env.HIEP_TEST_ENV,
+    NEXT_ENV: process.env.NEXT_ENV ? process.env.NEXT_ENV : "development",
   },
 };
