c9affaf5624d28a916b8b9655c790722485d685e
hiepnv <hiepnv@hblab.vn>
2021-02-26 11:47:55 +0700

    Check env

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
     <div className={styles.container}>
       <h1>process.env.NODE_ENV {process.env.NODE_ENV}</h1>
       <h1>process.env.CUSTOM {process.env.CUSTOM}</h1>
-      <h1>process.env.CHECK_ENV {process.env.CHECK_ENV}</h1>
+      <h1>process.env.NEXT_PUBLIC_CHECK_ENV {process.env.NEXT_PUBLIC_CHECK_ENV}</h1>
       <h1>process.env.NEXT_ENV {process.env.NEXT_ENV}</h1>
       Test env
     </div>
