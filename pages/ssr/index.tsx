00ce6cfbf09120cae6acf724ea95354cff21b21f
Hiep Nguyen Van <bka.vanhiep@gmail.com>
2021-05-04 08:40:58 +0700

    Test: Heroku deploy 500 error monooq test

--- pages/ssr/index.tsx
+++ pages/ssr/index.tsx
@@ -1,12 +1,14 @@
-import { GetServerSideProps } from 'next';
 import * as React from 'react';
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 
-export default function SSR () {
-  return <p>SSR</p>
+export default function TestPage({
+  blogs,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return <p>Test page {blogs}</p>;
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
   return {
-    props: {}
-  }
-}
+    props: { blogs: [] },
+  };
+};
