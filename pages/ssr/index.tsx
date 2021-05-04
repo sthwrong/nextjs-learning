11d11cfd5e477dbe04ce3758d18fa47f7bee39bc
Hiep Nguyen Van <bka.vanhiep@gmail.com>
2021-05-04 08:37:30 +0700

    Test SSR typescript deploy to Heroku

--- pages/ssr/index.tsx
+++ pages/ssr/index.tsx
@@ -1,5 +1,12 @@
+import { GetServerSideProps } from 'next';
 import * as React from 'react';
 
 export default function SSR () {
   return <p>SSR</p>
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    props: {}
+  }
 }
